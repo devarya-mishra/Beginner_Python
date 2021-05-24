d385c059c227f0d51f3daf17086aaf562e8c1865
devarya-mishra <65231468+devarya-mishra@users.noreply.github.com>
2021-05-25 00:49:10 +0530

    Add files via upload
    You must have heard a game named Housie, I hated that game, the reason is that a person has to pick a random number from 1 - 99 and if the number is in your ticket, its good for you. But sometimes people cheat in this game by partnering with the person who is picking the numbers. Thats why i made this program, so we could play it fair!!

---
+++ bingo.py
@@ -0,0 +1,28 @@
+import random
+remain = []
+picked = []
+for i in range(1, 100):
+    remain.append(i)
+for k in range(1, 1001):
+    user = input("Type ' >> ' to get a random number and Type ' << ' to check if a number is already picked(type exit to quit): ").lower()
+    if user == ">>":
+        b = random.choice(remain)
+        print(b)
+        remain.remove(b)
+        picked.append(b)
+        print(f"Number is {b}")
+        continue
+    elif user == "<<":
+        check = int(input("Enter the number to check if it is already picked: "))
+        if check in picked:
+            print(f"The number {check} has already been picked!!!")
+        elif check not in picked:
+            print("No the following number is not yet picked!!")
+        else:
+            print("Wrong input entered.")
+            break
+    elif user == 'exit':
+        print('program has ended.')
+        quit()
+    else:
+        print('sorry wrong input entered.')
